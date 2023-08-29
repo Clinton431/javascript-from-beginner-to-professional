95582ddc339fb5e6ddefcadda01e98c8e3b4a7f0
Clinton431 <clintonnyakoe@gmail.com>
2023-08-29 11:14:55 +0300

    chapter 1

---
+++ 1-getting-started/chapter-1-clinton-quiz.js
@@ -0,0 +1,11 @@
+let greeting = "Hello Javascript";
+
+console.log(greeting)
+
+/*
+This is a multiple line comment
+It is used to write comments in multiple lines
+It is better than single line comment because it makes the code more understandable
+*/
+
+//The best way to remove a line of code from running that you might want to keep as you debug is to comment the line of code.
