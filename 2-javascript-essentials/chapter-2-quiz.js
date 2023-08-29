751dd1298949b4647ed112e5f7708b8c3a29a8cd
wolofare <wolofare1@gmail.com>
2023-08-29 10:58:56 +0300

    Chapter 2 quiz exercises

---
+++ 2-javascript-essentials/chapter-2-quiz.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * BMI Calculator
+ * Description: Converting values of (height(inches) and weight (pounds)) to centimeters and kilos
+ *
+ * 1 inch = 2.54 cm
+ * 2.2046 pounds = 1 kilo
+ */
+
+let height = 5;
+let weight = 3.4;
+
+// BMI:
+convert_to_BMI = weight / height;
+
+console.log(convert_to_BMI);
